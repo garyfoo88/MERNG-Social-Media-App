0e5257bf7b47eff9a598344f7d959b4be566e6cd
garyfoo88 <reid9000200020002000@gmail.com>
2021-07-22 00:20:18 +0800

    added auth checker

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,5 +1,5 @@
 const Post = require("../../models/Post");
-
+const checkAuth = require("../../utils/checkAuth")
 module.exports = {
   Query: {
     async getPosts() {
@@ -27,7 +27,7 @@ module.exports = {
   },
   Mutation: {
     async createPost(_, { body }, context) {
-      
+      const user = checkAuth(context)
     },
   },
 };
--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,5 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { AuthenticationError } = require("apollo-server");
 
 module.exports = (context) => {
-    
-}
+  //context containts {...headers}
+  const authHeader = context.req.headers.authorization;
+  if (authHeader) {
+    //Bearer...
+    const token = authHeader.split("Bearer ")[1];
+    if (token) {
+      try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        return user;
+      } catch (err) {
+        throw new AuthenticationError("Invalid/Expired");
+      }
+    }
+    throw new Error("Authentication token error")
+  }
+  throw new Error("Authentication header must be provided")
+};
